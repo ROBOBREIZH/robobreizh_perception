c0ba6cfe76f2f7154fca1b6b3339b4ed3e302b87
celebo <cedriclebono@protonmail.com>
2021-04-02 10:58:45 +0200

    Update video_demo.py

--- src/video_demo.py
+++ src/video_demo.py
@@ -3,17 +3,14 @@ import cv2
 
 class VideoDemo:
     def __init__(self, size=608, source=0, detector=DetectRobocup(), freq=10):
-        self.size = 608
+        self.size = size
         self.source = source
         self.detector = detector
-        self.freq = 10
+        self.freq = freq
         self.detect_video()
 
     def detect_video(self):
-        shape = 608
         cap = cv2.VideoCapture(self.source)
-        #ret, frame = cap.read()
-        #msk_res = self.detector.resize(frame, self.size)
         d = 0
         f = 0
         while cap.isOpened():
@@ -22,10 +19,10 @@ class VideoDemo:
             if ret:
                 if (d % self.freq == 0):
                     f = f + 1
-                    resized = self.detector.resize(frame, shape)
+                    resized = self.detector.resize(frame, self.size)
                     res, arr_hands, arr_bags, arr_persons, arr_empty_chairs, arr_taken_chairs, features, ids = self.detector.detect_image(
                         resized)
-                    msk_res = self.detector.resize(res, shape)
+                    msk_res = self.detector.resize(res, self.size)
                     cv2.imshow('Demo', msk_res)
                     cv2.waitKey(3)
             else:
